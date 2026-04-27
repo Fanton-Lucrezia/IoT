e08250bf0d5849f71fd11b05e96f651f15389ade
Fanton-Lucrezia <10934221@itisrossi.vi.it>
2026-04-27 12:12:21 +0200

    risolto conflitto

--- backend-flask/flask_server.py
+++ backend-flask/flask_server.py
@@ -210,8 +210,4 @@ if __name__ == "__main__":
     print(f"  Admin: {ADMIN_USERNAME} / {ADMIN_PASSWORD}")
     print(f"  Codice segreto QR: {SECRET_CODE}")
     print(f"{'='*40}\n")
-
-
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
-    #app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
